6c73c1104d6033a5f2909c3e464f51f9ddcf4862
Rayed-Zakwan <rayed.zakwan@northsouth.edu>
2022-12-12 00:29:25 +0600

    Update test.c

--- test.c
+++ test.c
@@ -3,10 +3,13 @@
 int main(void)
 {
     int age;
-    
+
     printf("Enter Age: ");
+    
     scanf("%d", &age);
+
     printf("Age is %d!", age);
+    printf("\nHello World!");
 
     return 0;
 }
